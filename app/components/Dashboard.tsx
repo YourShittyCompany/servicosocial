60d348a2d6ecd5e3f846d27b6499d75c7d712ae9
Luis Almeida <git@luisalmeida.me>
2024-11-11 11:55:40 +0000

    Versão 0.0.3

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -19,13 +19,13 @@ export default function Dashboard() {
       onContextMenu={(e) => e.preventDefault()}
     >
       <div className="black h-1/2 bg-[#22c55e] p-0 relative">
-        <div className="coming_soon w-[85%] ml-[5%] absolute bottom-[-40px] text-white font-['Josefin_Slab'] text-[160px] md:text-[160px] sm:text-[72px] sm:bottom-[-18px] z-10">
+        <div className="coming_soon w-[85%] ml-[5%] absolute bottom-[-40px] text-white font-['Josefin_Slab'] text-[160px] lg:text-[160px] md:text-[120px] sm:text-[72px] xs:text-[56px] sm:bottom-[-18px] z-10">
           BREVE
         </div>
       </div>
 
       <div className="white h-1/2 bg-white p-0 relative">
-        <div className="coming_soon w-[85%] ml-[5%] absolute top-[-8px] text-[#0d9488] font-['Josefin_Slab'] text-[160px] md:text-[160px] sm:text-[72px] sm:top-[-4px] z-10">
+        <div className="coming_soon w-[85%] ml-[5%] absolute top-[-8px] text-[#0d9488] font-['Josefin_Slab'] text-[160px] lg:text-[160px] md:text-[120px] sm:text-[72px] xs:text-[56px] sm:top-[-4px] z-10">
           MENTE
           {blinkingDots}
         </div>
